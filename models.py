57887dadc4641a2de904955044cdfe27ca711abb
Pawan <pawan.dhojadhikari@gmail.com>
2025-05-19 22:27:10 +0545

    models update

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
 #comment
+from sqlalchemy import Column ,Integer,String,Boolean
+from sqlalchemy.sql.expression import null
+from .database import Base
+
+class Posts(Base):
+    __tablename__='posts'
+    
+    id=Column(Integer,primary_key=True, nullable=False)
+    title=Column(String,nullable=False)
+    content=Column(String,nullable=False)
+    published=Column(Boolean,default=True)
