7f848bed41c25471819d8abbe47ee1de962686f2
Pawan <pawan.dhojadhikari@gmail.com>
2025-05-24 16:51:57 +0545

    Post and delete post

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from ast import mod
-from fastapi import Depends, FastAPI ,Response, status,HTTPException
+from importlib.resources import contents
+from turtle import pos, title
+from fastapi import Depends, FastAPI ,Response, dependencies, status,HTTPException
 from fastapi.params import Body
+# from networkx import number_weakly_connected_components
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
@@ -40,49 +43,41 @@ while True:
     
 my_posts=[{"title":'title of post 1','content':'content of post 1','id':1},{"title":'Fav food','content':'pizza','id':2}]
 
-def post_search(id):
-    for p in my_posts:
-        if p["id"]==id:
-            return p
-        
-def post_search_index(id):
-    for i,p in enumerate(my_posts):
-        if p['id']==id:
-            return i
+
 
 @app.get("/")
 def root():
  return {"message":"Hello World"}
 
-@app.get("/sqlalchemy")
-def test_post(db: Session=Depends(get_db)):
-    return{"Status":"Sucess"}
 
 
 @app.get("/posts")
-def get_posts():
-   cursor.execute("""SELECT * FROM post """)
-   posts=cursor.fetchall()
+def get_posts(db: Session=Depends( get_db)):
+   posts=db.query(models.Posts).all()
    return posts
   
 
 
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-def create_post(post:Post):
-    cursor.execute("""INSERT INTO post (title,content) VALUES (%s,%s) RETURNING * """,(post.title,post.content))
-    new_post=cursor.fetchone()
-    conn.commit()
+def create_post(post:Post,db: Session=Depends(get_db) ):
+    # new_post=models.Posts(title=post.title,content=post.content,published=post.published)
+    new_post=models.Posts(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return new_post
+    
     
 
 
 
 @app.get("/post/{id}")
-def get_post(id:int): 
+def get_post(id:int,db: Session=Depends(get_db)): 
    
-    cursor.execute("""SELECT * FROM post WHERE id=%s""",(str(id)))
-    post=cursor.fetchone()
+
+    post=db.query(models.Posts).filter(models.Posts.id==id).first()
+   
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id={id} was not found")
@@ -90,29 +85,15 @@ def get_post(id:int):
 
 
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id:int):
-    cursor.execute("""DELETE FROM post WHERE id=%s RETURNING *""",(str(id),))
-    post=cursor.fetchone()
-    conn.commit()
-    if not post:
+def delete_post(id:int,db: Session=Depends(get_db)):
+    # cursor.execute("""DELETE FROM post WHERE id=%s RETURNING *""",(str(id),))
+    # post=cursor.fetchone()
+    # conn.commit()
+    del_post=db.query(models.Posts).filter(models.Posts.id==id)
+    if not del_post.one():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id={id} was not found")
+    del_post.delete(synchronize_session=False)
+    db.commit()
     
-    return post
-    
-    
-    
-@app.put("/posts/{id}")
-def update_post(id: int, payload: Post):
-    cursor.execute("""UPDATE post SET title=%s,content=%s,published=%s WHERE id=%s returning *""",(payload.title,payload.content,payload.published,str(id)))
-    post=cursor.fetchone()
-    conn.commit()
-    
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id {id} does not exist")
-    return post
-    
-    
-
-    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-#comment
-from sqlalchemy import Column ,Integer,String,Boolean
+from pickle import TRUE
+from annotated_types import Timezone
+from sqlalchemy import TIMESTAMP, Column ,Integer,String,Boolean, text ,false
 from .database import Base
 
 class Posts(Base):
@@ -8,4 +9,7 @@ class Posts(Base):
     id=Column(Integer,primary_key=True, nullable=False)
     title=Column(String,nullable=False)
     content=Column(String,nullable=False)
-    published=Column(Boolean,default=True)
+    published=Column(Boolean,server_default='True',nullable=False)
+    creared_at=Column(TIMESTAMP(timezone=TRUE),nullable=false,server_default=text('now()'))
+   
+
